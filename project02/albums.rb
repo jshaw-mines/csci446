0d95130a67010b642bf0fc72fdbe2ce80c369940
Joshua Shaw <jshaw@mines.edu>
2013-01-27 18:18:51 -0700

    form looks good, working on list

--- project02/albums.rb
+++ project02/albums.rb
@@ -1,9 +1,37 @@
 require 'rack'
  
-class HelloWorld
-  def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
-  end
+class AlbumApp
+	def call(env)
+		request = Rack::Request.new(env)
+		case request.path
+		when "/form" then render_form(request)
+		when "/list" then render_list(request)
+		else render_404
+		end
+	end
+
+	def render_form (request)
+		response = Rack::Response.new
+		File.open("form.html", "rb") do	|f|
+			response.write(f.read)
+			100.times do |n|
+				response.write("<option value=#{n+1}>#{n+1}</option> ")
+			end
+			response.write ("</select><input type=\"submit\" value=\"Display List\"/></form></body></html>")
+		end
+		response.finish
+	end
+
+	def render_list (request)
+		response = Rack::Response.new
+		File.open("top_100_albums")
+		response.finish
+	end
+
+	def render_404
+		[404, {"Contenet-Type" => "text/plain"}, ["GO AWAY... newb"]]
+	end
+	
 end
- 
-Rack::Handler::WEBrick.run HelloWorld.new, :Port => 8080
+
+Rack::Handler::WEBrick.run AlbumApp.new, :Port => 8080
