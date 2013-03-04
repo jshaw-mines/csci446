ea21b668faa776c69ac169232bfc2f150a032e8b
Joshua Shaw <jshaw@mines.edu>
2013-03-03 22:47:55 -0700

    Adding bolierplate for project 4

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '447910dea64d859c16ea4b6c7f8f3ec7e3bbb3757bbc4cfd56b236d95ffd60d44d7a7830127fefa98a63aad5682c814698249b4b6832f45738fe466d42ac449e'
