a96e5015b677c9da9f88d3ede5879e32c99981f3
Joshua Shaw <jshaw@mines.edu>
2013-03-04 09:16:51 -0700

    added basic css to products page

--- project04/db/seeds.rb
+++ project04/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create(title: 'Ruby', description: "sup newb", image_url: 'ruby.jpg', price: 50.00)
