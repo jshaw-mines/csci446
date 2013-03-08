24e5983b0325c2cddbc3733e40120e8afb970cb1
Joshua Shaw <jshaw@mines.edu>
2013-03-08 11:08:59 -0700

    functional tests pass

--- project04/app/controllers/carts_controller.rb
+++ project04/app/controllers/carts_controller.rb
@@ -81,7 +81,7 @@ class CartsController < ApplicationController
     @cart.destroy
 	session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Your cart is currently empty' }
+      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
       format.json { head :ok }
     end
   end
